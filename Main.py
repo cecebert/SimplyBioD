68c5145521ecc1e803ced909bc897232d928d836
cecebert <cesarebertone@gmail.com>
2024-10-31 10:12:09 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -38,6 +38,9 @@ def get_dc_hl(nuclide_name):
 st.title('Experiment details')
 
 with st.sidebar:
+    st.header('Welcome to SimplyBioD', divider='blue')
+    st.subheader('Open-source Biodistribution data analysis dashboard for radioactive ☢️ tracers. This web application features more than 80 nuclides useful for imaging and therapy scopes. You will find below a quickstart tutorial for the use of this webstie.')
+    with st.expander(label='Section explanation'):
     st.header('Quickstart tutorial', divider='blue')
     st.subheader('Experimenter information and experimental details')
     with st.expander(label='Section explanation'):
